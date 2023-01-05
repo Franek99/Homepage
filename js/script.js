9722fdc93f769c0d4499f02813237cb40e3985fc
Tomasz Franusik <franusiktomasz>
2023-01-05 13:31:16 +0100

    New functions added

--- js/script.js
+++ js/script.js
@@ -1,25 +1,49 @@
-console.log("Cześć!");
-
-let przycisk = document.querySelector(".js-przycisk");
-let img = document.querySelector(".js-img");
-let themeName1 = document.querySelector(".js-themeName1");
-
-przycisk.addEventListener("click", () => {
-    img.hidden = !img.hidden;
-
-    themeName1.innerText = img.hidden ? "pokaż" : "ukryj"
-});
-
-let button = document.querySelector(".js-button");
-let body = document.querySelector(".js-container");
-let themeName = document.querySelector(".js-themeName");
-
-button.addEventListener("click", () => {
-    body.classList.toggle("grey");
-
-    if (body.classList.contains("grey")) {
-        themeName.innerText = "jasne";
-    } else {
-        themeName.innerText = "szare";
+{
+    const welcome = () => {
+        console.log("Cześć!")
     }
-});
+
+
+    const toggleBackground = () => {
+        const body = document.querySelector(".js-container");
+        const themeName = document.querySelector(".js-themeName");
+
+        body.classList.toggle("grey");
+
+        if (body.classList.contains("grey")) {
+            themeName.innerText = "jasne";
+        } else {
+            themeName.innerText = "szare";
+        }
+
+    }
+
+    const hiddenImg = () => {
+        const img = document.querySelector(".js-img");
+        const themeName1 = document.querySelector(".js-themeName1");
+
+        themeName1.innerText = img.hidden ? "pokaż" : "ukryj"
+        img.hidden = !img.hidden;
+
+    }
+
+    const action = () => {
+        const przycisk = document.querySelector(".js-przycisk");
+        przycisk.addEventListener("click", hiddenImg);
+    }
+
+    const init = () => {
+        const button = document.querySelector(".js-button");
+        button.addEventListener("click", toggleBackground);
+
+        welcome();
+    }
+
+    action();
+    init();
+}
+
+
+
+
+
